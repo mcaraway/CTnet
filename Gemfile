e176da67dad8341702a69c95dcef40029c69c1af
Michael Caraway <michael.p.caraway@gmail.com>
2017-12-22 18:48:25 -0500

    added devise
    added devise

--- Gemfile
+++ Gemfile
@@ -44,3 +44,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'tiny_tds'
 gem 'activerecord-sqlserver-adapter'
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }, controllers: {
+        sessions: 'users/sessions'
+      }
+  root to: 'application#home'
+  
+  get 'pages/home'
+  get '/home', to: 'application#home', as: 'home'
+  
   resources :shipments
   resources :stores
   resources :order_item_attributes
   resources :order_items
-  resources :users
   resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
