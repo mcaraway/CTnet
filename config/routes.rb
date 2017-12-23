d65c7b74e6cfdb1f3826e1fdf5b88641980a4193
mcaraway <michael.p.caraway@gmail.com>
2017-12-22 21:20:26 -0500

    Adding bootstrap theme

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   resources :order_item_attributes
   resources :order_items
   resources :orders
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
