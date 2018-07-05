7b35c794167e25f6953180ff41b98212bd82ed3d
mcaraway <michael.p.caraway@gmail.com>
2018-07-05 19:12:09 -0400

    changed to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180612144701) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dbconnections", force: :cascade do |t|
     t.string   "adapter"
     t.string   "mode"
@@ -82,8 +85,8 @@ ActiveRecord::Schema.define(version: 20180612144701) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
