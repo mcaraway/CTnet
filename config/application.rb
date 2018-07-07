1c4b91eb7acfc28f7d87b9a8297016ad296056a6
mcaraway <michael.p.caraway@gmail.com>
2018-07-07 12:30:28 -0400

    add server static assets

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module CTnet
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.encoding = "utf-8"
-    
+    config.serve_static_assets = true
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
