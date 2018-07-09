ece0728f24cfc0af64512a8d9fb7f1cfca91e5a7
mcaraway <michael.p.caraway@gmail.com>
2018-07-08 23:21:43 -0400

    seed admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create (email: "michael@carawaytea.com", password: "Del1rious", password_confirmation: "Del1rious",
+  admin: 1)
