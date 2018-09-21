972e5f97445ffe2c5b89b4dbf9f3d5b34f8def78
mcaraway <michael.p.caraway@gmail.com>
2018-09-21 11:42:18 -0400

    Updated orders query

--- app/models/order.rb
+++ app/models/order.rb
@@ -36,6 +36,14 @@ class Order < ShipworksDbBase
             WHEN 'FT006V' THEN 'Shake It Baby Vanilla 10ct'
             WHEN 'FT007M' THEN 'Shake It Baby Matcha 20ct'
             WHEN 'FT007V' THEN 'Shake It Baby Vanilla 20ct'
+            WHEN 'FT009M-B' THEN 'Shake It Baby Matcha 20ct Bundle w/ FT FOUR (four week pack'
+            WHEN 'FT008M-B' THEN 'Shake It Baby Matcha 20ct Bundle w/ FT Four (four week pack) w/ FTPOP60)'
+            WHEN 'FT010M-B' THEN 'Shake It Baby Matcha 20ct Bundle w/ FT FOUR POP (lollipop)'
+            WHEN 'FT011M-B' THEN 'Shake It Baby Matcha 20ct Bundle w/ Shake It Baby Matcha 20ct'
+            WHEN 'FT009V-B' THEN 'Shake It Baby Vanilla 20ct Bundle w/ FT FOUR (four week pack'
+            WHEN 'FT008V-B' THEN 'Shake It Baby Vanilla 20ct Bundle w/ FT Four (four week pack) w/ FTPOP60)'
+            WHEN 'FT010V-B' THEN 'Shake It Baby Vanilla 20ct Bundle w/ FT FOUR POP (lollipop)'
+            WHEN 'FT011V-B' THEN 'Shake It Baby Vanilla 20ct Bundle w/ Shake It Baby Vanilla 20ct'
           END as RollupItemName, sum([OrderItem].quantity) as quantity")
          .joins("JOIN Shipment on Shipment.OrderID = [order].OrderID")
          .joins("JOIN [OrderItem] on [order].OrderID = [OrderItem].OrderID")
@@ -50,6 +58,14 @@ class Order < ShipworksDbBase
             WHEN 'FT006V' THEN 'Shake It Baby Vanilla 10ct'
             WHEN 'FT007M' THEN 'Shake It Baby Matcha 20ct'
             WHEN 'FT007V' THEN 'Shake It Baby Vanilla 20ct'
+            WHEN 'FT009M-B' THEN 'Shake It Baby Matcha 20ct Bundle w/ FT FOUR (four week pack'
+            WHEN 'FT008M-B' THEN 'Shake It Baby Matcha 20ct Bundle w/ FT Four (four week pack) w/ FTPOP60)'
+            WHEN 'FT010M-B' THEN 'Shake It Baby Matcha 20ct Bundle w/ FT FOUR POP (lollipop)'
+            WHEN 'FT011M-B' THEN 'Shake It Baby Matcha 20ct Bundle w/ Shake It Baby Matcha 20ct'
+            WHEN 'FT009V-B' THEN 'Shake It Baby Vanilla 20ct Bundle w/ FT FOUR (four week pack'
+            WHEN 'FT008V-B' THEN 'Shake It Baby Vanilla 20ct Bundle w/ FT Four (four week pack) w/ FTPOP60)'
+            WHEN 'FT010V-B' THEN 'Shake It Baby Vanilla 20ct Bundle w/ FT FOUR POP (lollipop)'
+            WHEN 'FT011V-B' THEN 'Shake It Baby Vanilla 20ct Bundle w/ Shake It Baby Vanilla 20ct'
           END")
       }
     end
@@ -61,7 +77,7 @@ class Order < ShipworksDbBase
          .joins("JOIN [Store] on [order].StoreID = [Store].StoreID")
           .where("[order].OrderDate >= ? and [order].OrderDate <= ?", start_date, end_date)
           .where("StoreName='Flat Tummy Tea'")
-          .where("[OrderItem].sku in ('FT002','FT004','FT005','FT005-A','FT006V','FT006M','FT007V','FT007M')")
+          .where("[OrderItem].sku in ('FT002','FT004','FT005','FT005-A','FT006V','FT006M','FT007V','FT007M','FT009M-B','FT008M-B','FT010M-B','FT011M-B','FT009V-B','FT008V-B','FT010V-B','FT011V-B')")
           .group("CONVERT(DATE,DATEADD(day, DATEDIFF(day, 0, OrderDate) /7*7, 0),101), [OrderItem].sku")
           .order("CONVERT(DATE,DATEADD(day, DATEDIFF(day, 0, OrderDate) /7*7, 0),101), [OrderItem].sku")
       }
